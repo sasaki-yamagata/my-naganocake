2c737f5487897a20140684d0a672fd5e80cf23c2
EC2 Default User <ec2-user@ip-172-31-83-54.ec2.internal>
2022-03-12 09:58:00 +0000

    [Start]4

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  
+  validates :name, presence:true
+  validates :image, presence:true
+  validates :introduction, presence:true
+  validates :tax_excluded_price, presence:true
+  validates :is_sale, presence:true
 end
